04af46a4f86e81e0e88eb5c0a17d2cd862d6d9e9
Ajay <ajaymarkana97@gmail.com>
2020-07-30 15:58:33 +0530

    Successsful running

--- MyFirstDapp/contracts/Election.sol
+++ MyFirstDapp/contracts/Election.sol
@@ -2,8 +2,34 @@
 pragma solidity >= 0.5.0 < 0.7.0;
 
 contract Election {
-  string public candidate;
-  constructor() public {
-    candidate = "Candidate 1";
+  struct Candidate{
+    uint id;
+    string name;
+    uint voteCount;
+  }
+
+  mapping(address => bool) public voters;
+  mapping(uint => Candidate) public candidates;
+  uint public candidatesCount;
+
+  event votedEvent(
+    uint indexed _candidateId
+  );
+  constructor() public{
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate(string memory _name) private{
+    candidatesCount ++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
+  function vote(uint _candidateId) public{
+    require(!voters[msg.sender], "not valid");
+    require(_candidateId > 0 && _candidateId <= candidatesCount,"not valid candidate");
+    voters[msg.sender] = true;
+    candidates[_candidateId].voteCount ++;
+    emit votedEvent(_candidateId);
   }
 }
